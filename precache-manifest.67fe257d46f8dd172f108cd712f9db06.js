7bd824f174a876663f02a01c84d0279925e8a65e
IsmaSL <ucmex_isma@hotmail.com>
2020-08-20 00:37:11 -0500

    Updates

---
+++ precache-manifest.67fe257d46f8dd172f108cd712f9db06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e977c631759b9e7aa3d95557c2b4f23c",
+    "url": "/BuscadorImg_React-JS/index.html"
+  },
+  {
+    "revision": "5c29b1bf5177523caef3",
+    "url": "/BuscadorImg_React-JS/static/css/main.4675a583.chunk.css"
+  },
+  {
+    "revision": "5c2c10e431dcce40d55b",
+    "url": "/BuscadorImg_React-JS/static/js/2.434c91cc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/BuscadorImg_React-JS/static/js/2.434c91cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c29b1bf5177523caef3",
+    "url": "/BuscadorImg_React-JS/static/js/main.897111a2.chunk.js"
+  },
+  {
+    "revision": "70dd3d568cd6f72dfa24",
+    "url": "/BuscadorImg_React-JS/static/js/runtime-main.845bcdd5.js"
+  }
+]);
