e2bceffe15f372b866cd1027708421252e56f4b8
IsmaSL <ucmex_isma@hotmail.com>
2020-08-20 00:41:08 -0500

    P4_01: Finalizado.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// @Autor: Ismael Santarrosa L
+// @Fecha: Agosto 2020
+// @App: Aplicación para buscar imágenes por medio de una API
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
